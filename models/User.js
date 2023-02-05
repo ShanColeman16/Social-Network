09ab60e6630139999257fdb5d6345e38f93ef536
unknown <shannoncoleman16@gmail.com>
2023-02-04 20:34:35 -0500

    added models

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model}  =require('mongoose');
+const userSchema =  new Schema(
+
+    {
+        username:{
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email:{
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+            match:[ /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$, 'Please enter a valid email address' ],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            }
+        ],
+        friends:    [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+    },
+
+);
