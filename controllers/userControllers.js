61cc59940e5734d592078bfea79315b945629d22
unknown <shannoncoleman16@gmail.com>
2023-02-05 15:17:00 -0500

    added delete friend funtion to user controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -48,5 +48,17 @@ addFriend(req, res) {
         :res.json(user)
      )  
      .catch(( err) => res.status(500).json(err));
-}
+},
+deleteFriend(req, res) {
+    User.findOneandUpdate(
+        { _id: req.parmas.userId },
+        { $addToSet: { friends: req.parmas.friendId } },
+        { new: true }
+    )
+    .then((user) =>
+        !user ? res.status(404).json({ message: "No user found with that Id"}) 
+        :res.json(user)
+     )  
+     .catch(( err) => res.status(500).json(err));
+};
 
