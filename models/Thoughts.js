076422ca55bcecf11bccd4d19e60bd1c016adccb
unknown <shannoncoleman16@gmail.com>
2023-02-04 23:02:36 -0500

    added thought schema

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -16,7 +16,43 @@ const reactionSchema = new Schema (
             type: Date,
             default: Date.now,
             get: createdAtVal => momnet(createdAtVal).format("MMM DD, YYYY [at] hh:mm"),
-        }
-    }
+        },
+        {
+            toJSON: {
+                virtuals: true,
+                getters: true
+            },
+            id: false
+        },
+     }
+    );   
 
-)
+    const ThoughtSchema = new Schema (
+        {
+            thoughtText: {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 280,
+            },
+            createAt {
+                type: Date,
+                default: Date.now,
+                get: createdAtVal => momnet(createdAtVal).format("MMM DD, YYYY [at] hh:mm"),
+            },
+            username: {
+                type: String,
+                required: true
+            },
+            reactions: [reactionSchema],    
+            {
+                toJSON: {
+                    virtuals: true,
+                    getters: true
+                },
+                id: false
+            }
+        };
+    )
+
+    const Thought = model('Thought', ThoughtSchema);
