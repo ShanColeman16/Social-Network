4ac70930f3eb235121604f1a99dfd87542b51133
unknown <shannoncoleman16@gmail.com>
2023-02-13 08:24:08 -0500

    made changes to index js for routes and indexjs

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
-
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+const apiRoutes = require('/api');
+router.use('/api', apiRoutes);
+router.use((req, res) => res.send('Error'));
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,8 @@
 const router = require('express').Router();
-const apiRoutes = require('/api');
-router.use('/api', apiRoutes);
-router.use((req, res) => res.send('Error'));
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
