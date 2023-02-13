a7e545d3417d79e8882b5a1d6a4dc2cac93554ab
unknown <shannoncoleman16@gmail.com>
2023-02-13 11:00:48 -0500

    made changes to fix thoughts js

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -2,58 +2,68 @@ const   {   Schema, model, Types   } = require('mongoose');
 const moment = require('moment');
 
 const reactionSchema = new Schema (
+{ 
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdVAtVal => moment(createdVAtVal).format( "MMM DD, YYYY" ),
+    },
+  },
+  {
+    toJSON : {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+)
+const thoughtSchema = new Schema (
+{
+   thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdVAtVal => moment(createdVAtVal).format( "MMM DD, YYYY" ),
+  },
+    },
     {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () = > new Types.ObjectId()
-        },
+    reactions: [reactionSchema],
+    },
+  {
+    toJSON : {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+)
 
-        reactionBody: {
-            type: String,
-            required: true
-        },
-        createAt {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => momnet(createdAtVal).format("MMM DD, YYYY [at] hh:mm"),
-        },
-        {
-            toJSON: {
-                virtuals: true,
-                getters: true
-            },
-            id: false
-        },
-     }
-    );   
-
-    const ThoughtSchema = new Schema (
-        {
-            thoughtText: {
-                type: String,
-                required: true,
-                minlength: 1,
-                maxlength: 280,
-            },
-            createAt {
-                type: Date,
-                default: Date.now,
-                get: createdAtVal => momnet(createdAtVal).format("MMM DD, YYYY [at] hh:mm"),
-            },
-            username: {
-                type: String,
-                required: true
-            },
-            reactions: [reactionSchema],    
-            {
-                toJSON: {
-                    virtuals: true,
-                    getters: true
-                },
-                id: false
-            }
-        };
-    )
+thoughtSchema.virtual('reactionCount')
+.get(function () {
+    return this.reactions.length;
+})
 
     const Thought = model('Thought', ThoughtSchema);
 
